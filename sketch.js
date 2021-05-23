b9a9d7893f29412ff3b7f0b4efd6700f72b9e86f
jenilo <jennylopezu@gmail.com>
2021-05-22 22:23:40 -0600

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,112 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	frameRate(5);
+}
+
+function draw() {
+	stroke('red');
+	//ecuPP(0,0,width,height);
+	dda(0,0,width,height);
+
+	stroke('blue');
+	//ecuPP(width,0,0,height);
+	dda(0,height,width,0);
+
+	stroke('yellow');
+	//ecuPP(0,height/2,width,height/2);
+	dda(0,height/2,width,height/2);
+
+	stroke('gray');
+	//ecuPP(width/2,0,width/2,height);
+	dda(width/2,0,width/2,height);
+
+}
+
+function ecuPP(x1,y1,x2,y2){
+	let x = x1;
+	let y = y1;
+	let stepX = 1;
+	let stepY = 1;
+	const dx = x2 - x1;
+	const dy = y2 - y1;
+
+	if (dx == 0) {
+		if (dy < 0) stepY = -1;
+
+		while(y != y2){
+			point(x,y);
+			y += stepY;
+		}
+	}
+	else{
+		const m = dy / dx;
+		const b = y1 - m * x1;
+
+		if (dx < 0) stepX = -1;
+		while(x != x2){
+			point(x,y);
+			x += stepX;
+			y = m * x + b;
+		}
+	}
+
+	
+}
+
+function dda(x1,y1,x2,y2){
+	let x = x1;
+	let y = y1;
+	let stepX = 1;
+	let stepY = 1;
+	const dx = x2 - x1;
+	const dy = y2 - y1;
+	const m = dy / dx;
+
+	if (m >= 0) {
+		if (m <= 1) {
+			if (dx > 0) {
+				while(x < x2){
+					point(x,y);
+					x++;
+					y = y + m;
+				}
+			}
+			else{
+				while(x > x2){
+					point(x,y);
+					x--;
+					y = y - m;
+				}
+			}
+		}
+		else{
+			while(y < y2){
+				point(x,y);
+				y++;
+				x = x + 1/ m;
+			}
+		}
+	}
+	else{
+		if (m <= -1) {
+			while(x > x2){
+				point(x,y);
+				x--;
+				y = y - m;
+			}
+		}
+		else{
+			while(y2 < y){
+				point(x,y);
+				y--;
+				x = x - 1/ m;
+			}
+		}
+	}
+
+
+}
+
+function br(){
+	
+}
