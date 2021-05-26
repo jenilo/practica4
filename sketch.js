fb6b30e29b08505c738eda668aef31f510371859
jenilo <jennylopezu@gmail.com>
2021-05-26 11:59:49 -0600

    mpc y mpe

--- sketch.js
+++ sketch.js
@@ -5,24 +5,35 @@ function setup() {
 
 function draw() {
 	stroke('red');
-	//ecuPP(0,0,width,height);
-	//dda(0,0,width,height);
-	br(0,0,width,height);
+	ecuPP(0,1,200,200);
+	dda(200,1,400,200);
+	br(400,1,600,200);
 
 	stroke('blue');
-	//ecuPP(width,0,0,height);
-	//dda(0,height,width,0);
-	br(0,height,width,0);
+	ecuPP(0,200,200,1);
+	dda(200,200,400,1);
+	br(400,200,600,1);
 
 	stroke('yellow');
-	//ecuPP(0,height/2,width,height/2);
-	//dda(0,height/2,width,height/2);
-	br(0,height/2,width,height/2);
+	ecuPP(0,100,200,100);
+	dda(200,100,400,100);
+	br(400,100,600,100);
 
 	stroke('gray');
-	//ecuPP(width/2,0,width/2,height);
-	//dda(width/2,0,width/2,height);
-	br(width/2,0,width/2,height);
+	ecuPP(100,0,100,200);
+	dda(300,0,300,200);
+	br(500,1,500,200);
+
+	stroke('black');
+	mpc(100,300,50);
+	mpe(300, 300, 25, 50);
+
+	noStroke();
+	text("ecuPP",90,220);
+	text("dda",290,220);
+	text("bresen",490,220);
+	text("mpc",90,380);
+	text("mpe",290,380);
 
 }
 
@@ -53,8 +64,6 @@ function ecuPP(x1,y1,x2,y2){
 			y = m * x + b;
 		}
 	}
-
-	
 }
 
 function dda(x1,y1,x2,y2){
@@ -107,8 +116,6 @@ function dda(x1,y1,x2,y2){
 			}
 		}
 	}
-
-
 }
 
 function br(x1,y1,x2,y2){
@@ -132,4 +139,71 @@ function br(x1,y1,x2,y2){
             y += sy;
         }
     }
+}
+
+function mpc(xc, yc, r){
+	let x = 0;
+	let y = r;
+	let p = Math.round(5/4 - r);
+	
+	printOct(xc, x, yc, y);
+
+	while(x < y){
+		x++;
+		if (p < 0) {
+			p = p + 2 * x +1;
+		}
+		else{
+			y--;
+			p = p + 2 * (x - y) + 1;
+		}
+		printOct(xc, x, yc, y);
+	}
+}
+
+function printOct(xc, x, yc, y){
+	printP1(xc, x, yc, y);
+	point(xc + y, yc + x);
+	point(xc - y, yc + x);
+	point(xc - y, yc - x);
+	point(xc + y, yc - x);
+}
+function printP1(xc, x, yc, y){
+	point(xc + x, yc + y);
+	point(xc + x, yc - y);
+	point(xc - x, yc + y);
+	point(xc - x, yc - y);
+}
+
+function mpe(xc, yc, rx, ry) {
+	let x = 0;
+	let y = ry;
+	let p1 = (ry * ry) - (rx * rx * ry)+ (0.25 * rx * rx);
+	let p2 = ((x + 0.5) * (x + 0.5) * ry * ry) + ((y - 1) * (y - 1) * rx * rx) - (rx * rx * ry * ry);
+
+	printP1(xc, x, yc, y);
+
+	while ((2 * ry * ry * x) <= (2 * rx * rx * y)) {
+		if (p1 < 0) {
+			x++;
+			p1 += (2 * ry * ry * x) + (ry * ry);
+		} else {
+			x++;
+			y--;
+			p1 += (2 * ry * ry * x) - (2 * rx * rx * y) + (ry * ry);
+		}
+		printP1(xc, x, yc, y);
+	}
+	
+	while (y != 0) {
+		if (p2 > 0) {
+			y += - 1;
+			p2 += - (2 * y * rx * rx) + (rx * rx);
+		} else {
+			x++;
+			y += - 1;
+			p2 += - (2 * y * rx * rx) + (2 * x * ry * ry) + (rx * rx) ;
+		}
+		printP1(xc, x, yc, y);
+	}
 }
